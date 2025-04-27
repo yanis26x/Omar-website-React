7e8efe3952add930bdeca85c176c7d741b4edb4c
yanis26x <yanis.djenadi@hotmail.com>
2025-04-26 21:24:17 -0400

    ajout de la banierre

---
+++ src/Composants/MAIN/BANNIERRE/Banniere.jsx
@@ -0,0 +1,8 @@
+import './Banniere.css';
+
+export default function Banniere() {
+  return (
+    <section className="banniere">
+    </section>
+  );
+}
--- src/Composants/MAIN/Main.jsx
+++ src/Composants/MAIN/Main.jsx
@@ -1,9 +1,13 @@
 import Header from "../HEADER/Header";
+import Banniere from "./BANNIERRE/Banniere";
+
 
 export default function Main() {
   return (
     <>
     <Header/>
+    <Banniere/>
+
     <h1>Couleurs + page .CSS + structure du HEADER a changer !</h1>
     </>
 
