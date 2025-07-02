a404fdd36d3482ad966ec5551169baeaf804bb36
yanis26x <yanis.djenadi@hotmail.com>
2025-07-02 05:17:46 -0400

    version pour voir si image marche

--- src/pages/Accueil.jsx
+++ src/pages/Accueil.jsx
@@ -5,14 +5,44 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 import ProduitCard from "../components/ProduitCard";
-import './accueil.css';
+import "./accueil.css";
 
 const produits = [
-  { id: 1, titre: "Musc Oud", prix: 19.99, image: "/assets/musc-oud.jpg",paymentURL: "https://buy.stripe.com/00gbJxchHgIXeLC8wz"},
-  { id: 2, titre: "Musc Blanc", prix: 19.99, image: "/assets/musc-blanc.jpg", paymentURL: "https://buy.stripe.com/fZe4h53Lb8cr46Y28c" },
-  { id: 3, titre: "Bakhour Oud - Cône", prix: 14.99, image: "/assets/bakhour-cone.jpg", paymentURL: "https://buy.stripe.com/28o9Bp0yZboD32U144" },
-  { id: 4, titre: "Bakhour Oud Blanc", prix: 19.99, image: "/assets/bakhour-blanc.jpg", paymentURL: "https://buy.stripe.com/5kA00P3Lb2S76f63cd" },
-  { id: 5, titre: "Bakhour Oud Standard", prix: 17.99, image: "/assets/bakhour-standard.jpg", paymentURL: "https://buy.stripe.com/aEU14T81r8crgTK6oq" },
+  {
+    id: 1,
+    titre: "Musc Oud",
+    prix: 19.99,
+    image: "/assets/musc-oud.jpg",
+    paymentURL: "https://buy.stripe.com/00gbJxchHgIXeLC8wz",
+  },
+  {
+    id: 2,
+    titre: "Musc Blanc",
+    prix: 19.99,
+    image: "/assets/musc-blanc.jpg",
+    paymentURL: "https://buy.stripe.com/fZe4h53Lb8cr46Y28c",
+  },
+  {
+    id: 3,
+    titre: "Bakhour Oud - Cône",
+    prix: 14.99,
+    image: "/assets/bakhour-cone.jpg",
+    paymentURL: "https://buy.stripe.com/28o9Bp0yZboD32U144",
+  },
+  {
+    id: 4,
+    titre: "Bakhour Oud Blanc",
+    prix: 19.99,
+    image: "/assets/bakhour-blanc.jpg",
+    paymentURL: "https://buy.stripe.com/5kA00P3Lb2S76f63cd",
+  },
+  {
+    id: 5,
+    titre: "Bakhour Oud Standard",
+    prix: 17.99,
+    image: "/assets/bakhour-standard.jpg",
+    paymentURL: "https://buy.stripe.com/aEU14T81r8crgTK6oq",
+  },
 ];
 
 export default function Accueil() {
@@ -25,39 +55,39 @@ export default function Accueil() {
     <div>
       {/* BANNIÈRE */}
       <section className="banniere">
-    <img src="./assets/IAimage2.png" alt="Bannière de RIADddd" className="background-image-fallback" />
-  <div className="banner-content">
-    <h1>Bienvenue chez RIAD</h1>
-    <button onClick={scrollToProduits} className="bouton">Voir Produits</button>
-  </div>
-</section>
+        <img
+          src={`${import.meta.env.BASE_URL}assets/IAimage2.png`} 
+          alt="Bannière de RIADddd"
+          className="background-image-fallback"
+        />
 
-
+        <div className="banner-content">
+          <h1>Bienvenue chez RIAD</h1>
+          <button onClick={scrollToProduits} className="bouton">
+            Voir Produits
+          </button>
+        </div>
+      </section>
 
       {/* PRODUITS */}
       <section id="produits">
         <h2>Nos Produits</h2>
-        <div style={{ 
-          display: "grid", 
-          gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))", 
-          gap: "2rem",
-          marginTop: "2rem",
-        }}>
-          {produits.map(produit => (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "repeat(auto-fit, minmax(200px, 1fr))",
+            gap: "2rem",
+            marginTop: "2rem",
+          }}
+        >
+          {produits.map((produit) => (
             <ProduitCard key={produit.id} produit={produit} />
           ))}
         </div>
       </section>
-
-
     </div>
   );
 }
-
-
-
-
-
 
 //v2
 // LE CSS de acceil2.css A UTILISER ! !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Omar-website-React/',
+  base: process.env.NODE_ENV === 'production' ? '/Omar-website-React/' : '/',
   plugins: [react()],
 })
